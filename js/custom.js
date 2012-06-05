f830bb3cc0627a3e14beb4501266ad0248a4697d
turingou <o.u.turing@gmail.com>
2012-06-06 02:20:19 +0800

    add seajs support , etc.
    add seams support
    add awesome font
    add 1140gs
    add cufon canvas-font
    add mustache support

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,26 @@
 // feel free to write some sth cool...
 define(function(require) {
+	require('jq');
+	require('tpl');
+	var demoData = {
+		'title' : 'Competently restore cross-platform',
+		'desc' : 'Holisticly engage superior communities and process-centric e-business. Distinctively aggregate ubiquitous collaboration and idea-sharing and scalable materials. Dynamically.',
+		'p' : 'Energistically integrate stand-alone growth strategies vis-a-vis vertical scenarios. Compellingly plagiarize web-enabled quality vectors rather than leading-edge alignments. Interactively productivate maintainable experiences with adaptive meta-services. Credibly coordinate.',
+		'a' : 'SignUp',
+		'copyright' : '&copy; 2012 Your Logo Here',
+		'nav' : {
+			'name' : 'Home',
+			'name' : 'About',
+			'name' : 'Contact'
+		}
+	}
+	$('.demo-data-title').append(Mustache.render('{{title}}', demoData));
+	$('.demo-data-nav').append(Mustache.render('{{#nav}}{{name}}{{/nav}}', demoData));
+	$('.demo-data-a').append(Mustache.render('{{a}}', demoData));
+	$('.demo-data-p').append(Mustache.render('{{p}}', demoData));
+	$('.demo-data-ft').append(Mustache.render('{{{copyright}}}', demoData));
+
+// IT IS A require.async TPL
 //	require.async('mp3' , function () {
 //		audiojs.events.ready(function() {
 //		    audiojs.createAll();
--- js/init.js
+++ js/init.js
@@ -1,11 +1,18 @@
 seajs.config({
   alias: {
+  	// libs
   	'jq' : 'http://code.jquery.com/jquery.min.js',
-  	'less': './less/less-1.3.0.min',
-  	// js plugins
+  	'less': './less/less-1.3.0.min', // write less css
+  	'd3':'./lib/d3.v2', // d3 data-driven canvas lib
+  	'cufon':'./lib/cufon-yui', // font creator
+  	'tpl':'./lib/mustache',
+  	// plugins
+  	'mas':'./plugins/jquery.masonry.min',
   	'bgi' : './plugins/jquery.backstretch.min',
   	'tiptip' : './plugins/tiptip/jquery.tiptip.min',
   	'mp3' : './plugins/audiojs/audio.min',
+  	'fancybox' : './plugins/fancybox/jquery.fancybox-1.3.4.pack',
+  	'placeholder':'./plugins/jquery.placeholder.min',
   	// tomato webdemo frame UI scripts
   	'fuc' : './function',
   	// custom scripts
